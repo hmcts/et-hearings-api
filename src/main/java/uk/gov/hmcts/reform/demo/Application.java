19b4696d4d71b60c901bf757d76b6d2124cd5f9d
jip1984 <Jon.Prosser@HMCTS.NET>
2023-05-19 14:12:25 +0100

    testing pipeline out this commit should fail on checkStyle

--- src/main/java/uk/gov/hmcts/reform/demo/Application.java
+++ src/main/java/uk/gov/hmcts/reform/demo/Application.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.demo;
 import org.springframework.boot.SpringApplication;
 
 
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
