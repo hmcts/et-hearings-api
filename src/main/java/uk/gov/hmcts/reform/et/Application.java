98e4126e09aa9f8283f9d422a3866ba159849508
jip1984 <Jon.Prosser@HMCTS.NET>
2023-05-19 14:43:17 +0100

    fixed checkstyle error for testing jenkins pipeline

--- src/main/java/uk/gov/hmcts/reform/et/Application.java
+++ src/main/java/uk/gov/hmcts/reform/et/Application.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.et;
 
 import org.springframework.boot.SpringApplication;
-
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
