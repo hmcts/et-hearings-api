a716e53c4499ecf9ca48203d3d424c4d609ced34
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-06-19 16:09:15 +0100

    add unit test

--- src/test/java/uk/gov/hmcts/reform/et/model/service/hearingvalues/ServiceHearingValuesTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/model/service/hearingvalues/ServiceHearingValuesTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.et.utils.ResourceLoader;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 class ServiceHearingValuesTest {
@@ -21,6 +22,20 @@ class ServiceHearingValuesTest {
 
         // assert all values are preserved after serializing/deserializing ignoring node order
         assertThat(MAPPER.readValue(deserializedJson, ServiceHearingValues.class)).isEqualTo(serializedObject);
+    }
+
+    @Test
+    void shouldHaveCorrectParameters() throws Exception {
+
+        String actualJson = ResourceLoader.loadJson("serviceHearingValues.json");
+        ServiceHearingValues serializedObject = MAPPER.readValue(actualJson, ServiceHearingValues.class);
+
+        assertFalse(serializedObject.isAutoListFlag(),"auto list flag");
+        assertThat(serializedObject.getPublicCaseName()).isEqualTo(" Johnny Claimant v Acme Redde Ltd");
+        assertThat(serializedObject.getCaseDeepLink()).isEqualTo("/documents/deep/link");
+        assertThat(serializedObject.getHmctsInternalCaseName()).isEqualTo("Johnny Claimant v Acme Redde Ltd");
+        assertThat(serializedObject.getCaseManagementLocationCode()).isEqualTo("location123");
+
     }
 
 }
