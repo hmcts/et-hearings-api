25479bc8e6f30a0c402f039857b57e9bc0976527
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-07-26 14:53:05 +0100

    add dependency to build.gradle

--- src/main/java/uk/gov/hmcts/reform/et/Application.java
+++ src/main/java/uk/gov/hmcts/reform/et/Application.java
@@ -4,15 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
 @SpringBootApplication
-@EnableFeignClients(basePackages = {"uk.gov.hmcts.reform"
-}
-)
+@EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.et"})
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
-public class HearingApiApplication {
 
-    public static void main(final String[] args) {
-        SpringApplication.run(HearingApiApplication.class, args);
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
+
 }
