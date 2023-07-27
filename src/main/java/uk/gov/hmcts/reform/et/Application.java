a7e125793c687b81ad85f2c9b036c260e466afae
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-07-27 09:22:57 +0100

    add httpClientConfiguration

--- src/main/java/uk/gov/hmcts/reform/et/Application.java
+++ src/main/java/uk/gov/hmcts/reform/et/Application.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform.et;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.openfeign.*;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.et"})
---
+++ src/main/java/uk/gov/hmcts/reform/et/config/ServiceTokenGeneratorConfiguration.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.reform.et.config;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
+
+@Configuration
+@Lazy
+public class ServiceTokenGeneratorConfiguration {
+
+    @Bean
+    @Primary
+    @Qualifier("ccd")
+    public AuthTokenGenerator authTokenGenerator(
+        @Value("${idam.s2s-auth.totp_secret}") String secret,
+        @Value("${idam.s2s-auth.microservice}") String microService,
+        ServiceAuthorisationApi serviceAuthorisationApi
+    ) {
+        return AuthTokenGeneratorFactory.createDefaultGenerator(secret, microService, serviceAuthorisationApi);
+    }
+
+    @Bean
+    @Qualifier("xui")
+    public AuthTokenGenerator authTokenGeneratorXui(
+        @Value("${idam.s2s-auth.totp_secret2}") String secret,
+        @Value("${idam.s2s-auth.microservice2}") String microService,
+        ServiceAuthorisationApi serviceAuthorisationApi
+    ) {
+        return AuthTokenGeneratorFactory.createDefaultGenerator(secret, microService, serviceAuthorisationApi);
+    }
+}
