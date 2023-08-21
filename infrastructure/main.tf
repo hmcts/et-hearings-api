4ea8fbb9f5a545cac5f20471995688f2b9a7c98c
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-08-21 16:46:54 +0100

    add kv infrastucture

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,74 @@
 provider "azurerm" {
   features {}
 }
+
+locals {
+  tagEnv = var.env == "aat" ? "staging" : var.env
+  tags = merge(var.common_tags,
+    tomap({
+      "environment" = local.tagEnv,
+      "managedBy" = var.team_name,
+      "Team Contact" = var.team_contact,
+      "application" = "employment-tribunals",
+      "businessArea" = "CFT",
+      "builtFrom" = "et-hearings-api"
+    })
+  )
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.component}-${var.env}"
+  location = var.location
+  tags     = local.tags
+}
+
+data "azurerm_user_assigned_identity" "et-identity" {
+  name                = "${var.product}-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name         = "${var.component}-POSTGRES-HOST"
+  value        = module.db.host_name
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name         = "${var.component}-POSTGRES-PORT"
+  value        = "5432"
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name         = "${var.component}-POSTGRES-DATABASE"
+  value        = module.db.postgresql_database
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name         = "${var.component}-POSTGRES-USER"
+  value        = module.db.user_name
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name         = "${var.component}-POSTGRES-PASS"
+  value        = module.db.postgresql_password
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+data "azurerm_key_vault" "s2s_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "et_hearings_api_s2s_key" {
+  name         = "microservicekey-et-hearings-api"
+  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+}
+
+resource "azurerm_key_vault_secret" "et_hearings_api_s2s_secret" {
+  name         = "et-hearings-api-s2s-secret"
+  value        = data.azurerm_key_vault_secret.et_hearings_api_s2s_key.value
+  key_vault_id = module.key-vault.key_vault_id
+}
