d155850b1f424444cd1ec4b3b708a285443497ea
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-08-23 10:19:08 +0100

    update main.tf, state.tf, variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,16 +1,45 @@
-variable "product" {}
+variable "product" {
+  default = "et"
+}
 
-variable "component" {}
+variable "component" {
+  default = "hearings-api"
+}
 
 variable "location" {
   default = "UK South"
 }
 
-variable "env" {}
+variable "location_db" {
+  default = "UK South"
+}
 
-variable "subscription" {}
+variable "env" {
+}
+
+variable "subscription" {
+}
+
+variable "tenant_id" {}
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "deployment_namespace" {
+  default = ""
+}
 
 variable "common_tags" {
   type = map(string)
 }
 
+variable "team_name" {
+  description = "Team name"
+  default     = "Employment Tribunals"
+}
+
+variable "team_contact" {
+  description = "Team contact"
+  default     = "#et-devs"
+}
