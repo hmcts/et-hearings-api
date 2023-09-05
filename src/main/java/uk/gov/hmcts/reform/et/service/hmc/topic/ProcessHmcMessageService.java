43e5f1178c57e1e82dded8cfe4d95796d71cb41c
jip1984 <Jon.Prosser@HMCTS.NET>
2023-09-05 16:28:15 +0100

    service bus listener-jms implementation

---
+++ src/main/java/uk/gov/hmcts/reform/et/service/hmc/topic/ProcessHmcMessageService.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.et.service.hmc.topic;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.et.model.hmc.message.HmcMessage;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProcessHmcMessageService {
+
+    public void processEventMessage(HmcMessage hmcMessage){
+
+    }
+}
