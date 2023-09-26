7ad818ec2fb686c43393ab33888326f8f97c7e72
jip1984 <Jon.Prosser@HMCTS.NET>
2023-09-26 16:22:52 +0100

    fix checkstyle test in HmcHearingApiServiceTest import statements in wrong order

--- src/main/java/uk/gov/hmcts/reform/et/service/HmcHearingApi.java
+++ src/main/java/uk/gov/hmcts/reform/et/service/HmcHearingApi.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.et.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,7 +11,6 @@ import uk.gov.hmcts.reform.et.model.hearing.HearingGetResponse;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @FeignClient(name = "hmc-hearing", url = "${hmc.url}", configuration = FeignClientConfig.class)
-@Component
 public interface HmcHearingApi {
 
     String SERVICE_AUTHORIZATION = "ServiceAuthorization";
