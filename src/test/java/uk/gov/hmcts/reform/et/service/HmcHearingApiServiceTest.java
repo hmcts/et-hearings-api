413eb98fce6d8b871e09091ce2dc3cf0c3c07d14
jip1984 <Jon.Prosser@HMCTS.NET>
2023-09-26 16:16:20 +0100

    fix checkstyle test in HmcHearingApiServiceTest

--- src/test/java/uk/gov/hmcts/reform/et/service/HmcHearingApiServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/service/HmcHearingApiServiceTest.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.et.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
