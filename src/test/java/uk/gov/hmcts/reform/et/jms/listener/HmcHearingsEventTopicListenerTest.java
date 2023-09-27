af398c4f7dee1710069d5e2890b8032d59f0c181
jip1984 <Jon.Prosser@HMCTS.NET>
2023-09-27 12:09:38 +0100

    checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/et/jms/listener/HmcHearingsEventTopicListenerTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/jms/listener/HmcHearingsEventTopicListenerTest.java
@@ -24,7 +24,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOf
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.et.model.hmc.reference.HmcStatus.ADJOURNED;
 
 
