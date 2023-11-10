00c0d17640da90455ceb525e3fbed17187056bca
Jack Reeve <jack@reevies.tech>
2023-11-10 17:05:35 +0000

    tmp

--- src/main/java/uk/gov/hmcts/reform/et/helper/mapping/HearingsPartyMapping.java
+++ src/main/java/uk/gov/hmcts/reform/et/helper/mapping/HearingsPartyMapping.java
@@ -1,17 +1,25 @@
 package uk.gov.hmcts.reform.et.helper.mapping;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.ObjectUtils;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.et.common.model.ccd.items.RepresentedTypeRItem;
 import uk.gov.hmcts.et.common.model.ccd.items.RespondentSumTypeItem;
+import uk.gov.hmcts.et.common.model.ccd.types.ClaimantHearingPreference;
+import uk.gov.hmcts.et.common.model.ccd.types.Organisation;
 import uk.gov.hmcts.et.common.model.ccd.types.RepresentedTypeC;
+import uk.gov.hmcts.et.common.model.ccd.types.RepresentedTypeR;
 import uk.gov.hmcts.et.common.model.ccd.types.RespondentSumType;
 import uk.gov.hmcts.et.common.model.hmc.IndividualDetails;
+import uk.gov.hmcts.et.common.model.hmc.OrganisationDetails;
 import uk.gov.hmcts.et.common.model.hmc.PartyDetails;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import static org.apache.commons.lang3.StringUtils.defaultString;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
@@ -88,12 +96,24 @@ public final class HearingsPartyMapping {
 
     private static PartyDetails mapPartyDetailsForRespondentRep(RepresentedTypeRItem repItem) {
         // TODO: Do we need IndividualDetails for RespondentRep?
-        return PartyDetails.builder()
+        RepresentedTypeR rep = repItem.getValue();
+        PartyDetails partyDetails = PartyDetails.builder()
                 .partyID(repItem.getId())
-                .partyName(repItem.getValue().getNameOfRepresentative())
+                .partyName(rep.getNameOfRepresentative())
                 .partyRole(LEGAL_REPRESENTATIVE.getHmcReference())
-                .partyType(isEmpty(repItem.getValue().getNameOfOrganisation()) ? INDIVIDUAL : ORGANISATION)
+                .partyType(isEmpty(rep.getNameOfOrganisation()) ? INDIVIDUAL : ORGANISATION)
                 .build();
+
+        if (StringUtils.isEmpty(rep.getNameOfOrganisation())) {
+            partyDetails.setIndividualDetails();
+        } else {
+            Organisation organisation = rep.getRespondentOrganisation();
+            partyDetails.setOrganisationDetails(OrganisationDetails.builder()
+                    .name(rep.getNameOfOrganisation())
+                    .cftOrganisationID(ObjectUtils.isEmpty(organisation) ? null : organisation.getOrganisationID())
+                    .build());
+        }
+        return partyDetails;
     }
 
     private static PartyDetails mapPartyDetailsForClaimant(CaseData caseData) {
@@ -117,7 +137,7 @@ public final class HearingsPartyMapping {
         RepresentedTypeC claimantType = caseData.getRepresentativeClaimantType();
 
         // TODO: Do we need IndividualDetails for ClaimantRep?
-        return PartyDetails.builder()
+        PartyDetails partyDetails = PartyDetails.builder()
                 // TODO: Remove the defaultString line when migration has gone live (RET-4383)
                 // .partyID(claimantType.getRepresentativeId())
                 .partyID(defaultString(claimantType.getRepresentativeId(), UUID.randomUUID().toString()))
@@ -125,6 +145,45 @@ public final class HearingsPartyMapping {
                 .partyRole(LEGAL_REPRESENTATIVE.getHmcReference())
                 .partyType(isEmpty(claimantType.getNameOfOrganisation()) ? INDIVIDUAL : ORGANISATION)
                 .build();
+
+        if (StringUtils.isEmpty(claimantType.getNameOfOrganisation())) {
+            partyDetails.setIndividualDetails(mapIndividualDetailsForClaimantRep(claimantType));
+        } else {
+            partyDetails.setOrganisationDetails(OrganisationDetails.builder()
+                    .name(claimantType.getNameOfOrganisation())
+                    .build());
+        }
+
+        return partyDetails;
+    }
+
+    private static IndividualDetails mapIndividualDetailsForClaimantRep(RepresentedTypeC claimantType) {
+        return IndividualDetails.builder()
+                .firstName(getFirstName(claimantType.getNameOfRepresentative()))
+                .lastName(getLastName(claimantType.getNameOfRepresentative()))
+                .preferredHearingChannel(claimantType.getRepresentativePreference())
+                .hearingChannelEmail(List.of(claimantType.getRepresentativeEmailAddress()))
+                .hearingChannelPhone(
+                        Stream.of(claimantType.getRepresentativePhoneNumber(),
+                                        claimantType.getRepresentativeMobileNumber())
+                                .filter(Objects::nonNull)
+                                .toList())
+                .custodyStatus("A")
+                .build();
+    }
+
+    private static String getFirstName(String name) {
+        if (name.contains(" ")) {
+            return name.substring(0, name.indexOf(' '));
+        }
+        return name;
+    }
+
+    private static String getLastName(String name) {
+        if (name.contains(" ")) {
+            return name.substring(name.indexOf(' ') + 1);
+        }
+        return name;
     }
 }
 
