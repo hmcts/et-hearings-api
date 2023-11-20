119f1cc49e9c9eafaad73c0b6001ed851ba076c8
Jack Reeve <jack@reevies.tech>
2023-11-20 10:21:17 +0000

    Extending PartyDetails objects in ServiceHearingValues

--- src/test/java/uk/gov/hmcts/reform/et/helper/mapping/HearingsPartyMappingTest.java
+++ src/test/java/uk/gov/hmcts/reform/et/helper/mapping/HearingsPartyMappingTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.et.common.model.ccd.types.RespondentSumType;
+import uk.gov.hmcts.et.common.model.hmc.IndividualDetails;
 import uk.gov.hmcts.et.common.model.hmc.PartyDetails;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.et.model.CaseTestData;
 import uk.gov.hmcts.reform.et.model.service.hearingvalues.ServiceHearingValues;
 
@@ -13,23 +13,48 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class HearingsPartyMappingTest {
-    CaseDetails mockCaseDetails;
     ServiceHearingValues serviceHearingValues;
     CaseData caseData;
 
     @BeforeEach
     void setUp() throws IOException, URISyntaxException {
-        mockCaseDetails = new CaseTestData().expectedDetails();
-        serviceHearingValues = new CaseTestData().expectedServiceHearingValues();
-        caseData = CaseDataMapping.mapCaseData(mockCaseDetails.getData());
+        CaseTestData caseTestData = new CaseTestData();
+        serviceHearingValues = caseTestData.expectedServiceHearingValues();
+        caseData = caseTestData.getResource("responses/caseDataPartyMappingFull.json", CaseData.class);
     }
 
     @Test
     void testBuildPartyObjectForHearingPayloadFullPayload() {
         List<PartyDetails> parties = HearingsPartyMapping.buildPartyObjectForHearingPayload(caseData);
+
+        assertEquals(6, parties.size());
+
+        PartyDetails claimant = parties.get(0);
+
+        PartyDetails expectedClaimant = PartyDetails.builder()
+                .partyID(caseData.getClaimantId())
+                .partyType("IND")
+                .partyRole("CLAI")
+                .partyName("Citizen One")
+                .individualDetails(IndividualDetails.builder()
+                        .title("Other")
+                        .firstName("Citizen")
+                        .lastName("One")
+                        .preferredHearingChannel("Email")
+                        .interpreterLanguage("nld")
+                        .custodyStatus("A")
+                        .vulnerableFlag(true)
+                        .vulnerabilityDetails("vulnerable with reason")
+                        .hearingChannelEmail(List.of("et.citizen1@hmcts.net"))
+                        .hearingChannelPhone(List.of("07111111111"))
+                        .build())
+                .build();
+
+        assertEquals(expectedClaimant, claimant);
         // assertEquals(serviceHearingValues.getParties(), parties);
         assertNotNull(parties);
     }
